dd996ed6a7f29d392f0c9500a4338d3c479f0a2f
Dmitriy Nikiforov <nikiforov.psy@gmail.com>
2020-06-28 18:27:32 +0300

    Updates

---
+++ precache-manifest.ff5e22bcee5cccc458dacf48e60a3589.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "597de7d2f5737457c0e5eefcae5c8ce8",
+    "url": "/umapp/index.html"
+  },
+  {
+    "revision": "6ca4fd3b9bd8dd145ffc",
+    "url": "/umapp/static/css/main.6dce3bfc.chunk.css"
+  },
+  {
+    "revision": "1073f64b74f2e0147be0",
+    "url": "/umapp/static/js/2.6279aae6.chunk.js"
+  },
+  {
+    "revision": "49d0a04bc698edcfe51cdbdc8cb4adbc",
+    "url": "/umapp/static/js/2.6279aae6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ca4fd3b9bd8dd145ffc",
+    "url": "/umapp/static/js/main.51104ce9.chunk.js"
+  },
+  {
+    "revision": "534fc5d10298ea854db2",
+    "url": "/umapp/static/js/runtime-main.938fca66.js"
+  }
+]);
